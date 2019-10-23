3bf75e661c0e28f98e99834f976f578ab14809b8
Vyctrus <koniokradx@gmail.com>
2019-10-23 14:43:54 +0200

    Ja Wielki Vyctrus Pan i Władca Mroku zatwierdzam te oto zmiany w tym projekcie

--- Form1.cs
+++ Form1.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Modbus;
+
 using System.Windows.Forms;
+using EasyModbus;
 
 namespace ParamSieci19
 {
     public partial class Form1 : Form
     {
+
+        public void connectAndrzej()
+        {
+            ModbusClient modbusX = new ModbusClient("127.0.0.1", 502);
+            modbusX.Connect();
+            int[] readHoldingRegisters = modbusX.ReadHoldingRegisters(0, 10);
+            foreach (int i in readHoldingRegisters)
+            {
+                Debug.WriteLine("Value of register " + i + "is" + readHoldingRegisters[i] + "\n");
+            }
+            
+        }
         public Form1()
         {
             InitializeComponent();
@@ -21,6 +35,7 @@ namespace ParamSieci19
         private void button1_Click(object sender, EventArgs e)
         {
             textBox1.Text = "ala ma kota";
+
         }
     }
 }
